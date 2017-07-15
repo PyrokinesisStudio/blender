PlumpMath/blender-2

Rename MT_[Vector/Matrix/Quaternion/Transform] to mt::[vec/mat/quat/trans].

source/gameengine/Rasterizer/RAS_MeshObject.cpp
@@ -59,9 +59,9 @@ struct RAS_MeshObject::polygonSlot
 
 	/* pnorm is the normal from the plane equation that the distance from is
 	 * used to sort again. */
-	void get(RAS_IDisplayArray *array, int offset, int nvert, const MT_Vector3& pnorm)
+	void get(RAS_IDisplayArray *array, int offset, int nvert, const mt::vec3& pnorm)
 	{
-		MT_Vector3 center(0.0f, 0.0f, 0.0f);
+		mt::vec3 center(0.0f, 0.0f, 0.0f);
 
 		for (unsigned short i = 0; i < nvert; ++i) {
 			m_index[i] = array->GetIndex(offset + i);
@@ -261,11 +261,11 @@ RAS_Polygon *RAS_MeshObject::AddPolygon(RAS_MeshMaterial *meshmat, int numverts,
 
 unsigned int RAS_MeshObject::AddVertex(
 				RAS_MeshMaterial *meshmat,
-				const MT_Vector3& xyz,
-				const MT_Vector2 * const uvs,
-				const MT_Vector4& tangent,
+				const mt::vec3& xyz,
+				const mt::vec2 * const uvs,
+				const mt::vec4& tangent,
 				const unsigned int *rgba,
-				const MT_Vector3& normal,
+				const mt::vec3& normal,
 				const bool flat,
 				const unsigned int origindex)
 {
@@ -420,7 +420,7 @@ void RAS_MeshObject::GenerateAttribLayers()
 	}
 }
 
-void RAS_MeshObject::SortPolygons(RAS_MeshSlot *ms, const MT_Transform &transform, unsigned int *indexmap)
+void RAS_MeshObject::SortPolygons(RAS_MeshSlot *ms, const mt::trans &transform, unsigned int *indexmap)
 {
 	// Limitations: sorting is quite simple, and handles many
 	// cases wrong, partially due to polygons being sorted per
@@ -452,7 +452,7 @@ void RAS_MeshObject::SortPolygons(RAS_MeshSlot *ms, const MT_Transform &transfor
 		return;
 
 	// Extract camera Z plane...
-	const MT_Vector3 pnorm(transform.getBasis()[2]);
+	const mt::vec3 pnorm(transform.getBasis()[2]);
 	// unneeded: const float pval = transform.getOrigin()[2];
 
 	std::vector<polygonSlot> poly_slots(totpoly);

PyrokinesisStudio/blender

source/gameengine/Rasterizer/RAS_MeshObject.cpp
@@ -59,9 +59,9 @@ struct RAS_MeshObject::polygonSlot
 
 	/* pnorm is the normal from the plane equation that the distance from is
 	 * used to sort again. */
-	void get(RAS_IDisplayArray *array, int offset, int nvert, const MT_Vector3& pnorm)
+	void get(RAS_IDisplayArray *array, int offset, int nvert, const mt::vec3& pnorm)
 	{
-		MT_Vector3 center(0.0f, 0.0f, 0.0f);
+		mt::vec3 center(0.0f, 0.0f, 0.0f);
 
 		for (unsigned short i = 0; i < nvert; ++i) {
 			m_index[i] = array->GetIndex(offset + i);
@@ -244,11 +244,11 @@ RAS_Polygon *RAS_MeshObject::AddPolygon(RAS_MeshMaterial *meshmat, int numverts,
 
 unsigned int RAS_MeshObject::AddVertex(
 				RAS_MeshMaterial *meshmat,
-				const MT_Vector3& xyz,
-				const MT_Vector2 * const uvs,
-				const MT_Vector4& tangent,
+				const mt::vec3& xyz,
+				const mt::vec2 * const uvs,
+				const mt::vec4& tangent,
 				const unsigned int *rgba,
-				const MT_Vector3& normal,
+				const mt::vec3& normal,
 				const bool flat,
 				const unsigned int origindex)
 {
@@ -407,7 +407,7 @@ void RAS_MeshObject::GenerateAttribLayers()
 	}
 }
 
-void RAS_MeshObject::SortPolygons(RAS_IDisplayArray *array, const MT_Transform &transform, unsigned int *indexmap)
+void RAS_MeshObject::SortPolygons(RAS_IDisplayArray *array, const mt::trans &transform, unsigned int *indexmap)
 {
 	// Limitations: sorting is quite simple, and handles many
 	// cases wrong, partially due to polygons being sorted per
@@ -437,7 +437,7 @@ void RAS_MeshObject::SortPolygons(RAS_IDisplayArray *array, const MT_Transform &
 		return;
 
 	// Extract camera Z plane...
-	const MT_Vector3 pnorm(transform.getBasis()[2]);
+	const mt::vec3 pnorm(transform.getBasis()[2]);
 	// unneeded: const float pval = transform.getOrigin()[2];
 
 	std::vector<polygonSlot> poly_slots(totpoly);