PlumpMath/blender-2

Rename mt::trans to mt::mat4x3.

source/gameengine/Rasterizer/RAS_MeshObject.cpp
@@ -420,7 +420,7 @@ void RAS_MeshObject::GenerateAttribLayers()
 	}
 }
 
-void RAS_MeshObject::SortPolygons(RAS_MeshSlot *ms, const mt::trans &transform, unsigned int *indexmap)
+void RAS_MeshObject::SortPolygons(RAS_MeshSlot *ms, const mt::mat4x3 &transform, unsigned int *indexmap)
 {
 	// Limitations: sorting is quite simple, and handles many
 	// cases wrong, partially due to polygons being sorted per

source/gameengine/Rasterizer/RAS_MeshObject.h
@@ -168,7 +168,7 @@ public:
 	void GenerateAttribLayers();
 
 	// polygon sorting by Z for alpha
-	void SortPolygons(RAS_MeshSlot *ms, const mt::trans &transform, unsigned int *indexmap);
+	void SortPolygons(RAS_MeshSlot *ms, const mt::mat4x3 &transform, unsigned int *indexmap);
 
 	bool HasColliderPolygon();
 

PyrokinesisStudio/blender

source/gameengine/Rasterizer/RAS_MeshObject.cpp
@@ -407,7 +407,7 @@ void RAS_MeshObject::GenerateAttribLayers()
 	}
 }
 
-void RAS_MeshObject::SortPolygons(RAS_IDisplayArray *array, const mt::trans &transform, unsigned int *indexmap)
+void RAS_MeshObject::SortPolygons(RAS_IDisplayArray *array, const mt::mat4x3 &transform, unsigned int *indexmap)
 {
 	// Limitations: sorting is quite simple, and handles many
 	// cases wrong, partially due to polygons being sorted per

source/gameengine/Rasterizer/RAS_MeshObject.h
@@ -164,7 +164,7 @@ public:
 	void GenerateAttribLayers();
 
 	// polygon sorting by Z for alpha
-	void SortPolygons(RAS_IDisplayArray *array, const mt::trans &transform, unsigned int *indexmap);
+	void SortPolygons(RAS_IDisplayArray *array, const mt::mat4x3 &transform, unsigned int *indexmap);
 
 	bool HasColliderPolygon();
 